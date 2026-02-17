8e2efa743b6297bc58c6ab12645b12e9191bb0c3
tasnim-echo <tasnim@echologyx.com>
2026-02-17 20:15:23 +0600

    done for mobile screen

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,12 @@ dropdownItem.addEventListener("click", function () {
   dropdownMenu.classList.toggle("active");
   dropdownItem.classList.toggle("active-color");
 });
+
+const mobileData = document.querySelector(".nav_data");
+const navMenu = document.querySelector(".nav_menu");
+
+mobileData.addEventListener("click", function () {
+  mobileData.classList.toggle("show_icon");
+
+  navMenu.classList.toggle("show_menu");
+});
